99aa741a0da9552e832eddfa452e80982083bf03
King Macking <matiasatzori@gmail.com>
2023-02-08 12:48:46 -0300

    Go home btn created

--- src/pages/PokemonQuizz/PokemonQuizz.jsx
+++ src/pages/PokemonQuizz/PokemonQuizz.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 import './PokemonQuizz.scss'
 
 const POKEMON = [
@@ -58,16 +59,23 @@ const PokemonQuizz = () => {
                 <label for="name">Ingresa el nombre del pokemon</label>
                 <input autoFocus className={`nes-input ${correct && "is-success"} ${incorrect && "is-error"}`} type="text" id="name" name="pokemon"/>
                 {incorrect && <p className="nes-text is-error">El pokemon ingresado es incorrecto</p>}
-                {!correct ? <button className="nes-btn is-primary" type="submit">Adivinar</button> :
+                {!correct ?
+                    <>
+                        <button className="nes-btn is-primary" type="submit">Adivinar</button>
+                        <Link className='nes-btn is-warning' to="/">Volver a inicio</Link>
+                    </>
+                :
                     <>
                         <p className="nes-text is-success">¡Muy bien, el pokemon era {POKEMON[POKEID]}!</p>
                         <button onClick={()=> location.reload()} className="nes-btn is-success" type="submit">Volver a jugar</button>
+                        <Link className='nes-btn is-warning' to="/">Volver a inicio</Link>
                     </>
                 }
             </form>
             <div className="pokequizz-img-pokemon">
                 <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${POKEID + 1}.png`} style={{imageRendering: "pixelated", filter: correct ? "" : "brightness(0) invert(1)"}}/>
             </div>
+            
         </main>
     )
 }
