fdf07de81a1e38872a4e25a517f0b4f39af490e2
King Macking <matiasatzori@gmail.com>
2023-02-08 00:25:10 -0300

    Memory game finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Dashboard from "./pages/Dashboard"
+import MemoryGame from "./pages/MemoryGame/MemoryGame"
 import PokemonQuizz from "./pages/PokemonQuizz/PokemonQuizz"
+import WordsPerMinute from "./pages/WordsPerMinute/WordsPerMinute"
 
 function App() {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/pokemonquizz" element={<PokemonQuizz/>}/>
+                <Route path="/" element={<Dashboard />}/>
+                <Route path="/pokemonquizz" element={<PokemonQuizz />}/>
+                <Route path="/memorygame" element={<MemoryGame />}/>
+                <Route path="/wordsxminute" element={<WordsPerMinute />}/>
             </Routes>
         </BrowserRouter>
     )
